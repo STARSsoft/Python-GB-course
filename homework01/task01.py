2f83203795d0e4f7d84a60198273a0954d861b55
mrivs <valentinilyins@gmail.com>
2023-08-27 17:28:13 +0300

    hw 01 added

--- homework01/task01.py
+++ homework01/task01.py
@@ -1,1 +1,25 @@
 # Решить задачи, которые не успели решить на семинаре.
+# Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
+
+def formatter(a, b):
+    c = a*b
+    if (c < 10):
+        return f"{b} x {a} =  {c}"
+    elif (a < 10):
+        return f"{b} x {a} = {c}"
+    else:
+        return f"{b} x {a}= {c}"
+
+
+print("\n \t \t ТАБЛИЦА УМНОЖЕНИЯ \n")
+
+for a in range(2, 11):
+    for b in range(2, 6):
+        print(formatter(a, b), end="\t")
+    print()
+print()
+for a in range(2, 11):
+    for b in range(6, 10):
+        print(formatter(a, b), end="\t")
+    print()
+print()
